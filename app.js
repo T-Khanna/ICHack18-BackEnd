f1665c5ca1001f4aba5e4c50559924ed28b2d6a6
Ioannis Gabrielides <ioannisgabrielides@gmail.com>
2018-01-27 17:33:29 +0000

    basic user connect/disconnect/message functionality

--- app.js
+++ app.js
@@ -6,16 +6,14 @@ var io = require('socket.io')(port);
 var fs = require('fs');
 
 io.on('connection', function (socket) {
-    io.emit('this', { will: 'be received by everyone'});
+  console.log("user connected");
+  socket.emit('server-message', 'nice to meet you');
 
-    console.log("connected");
+  socket.on('message', function (from, msg) {
+    console.log('message by ', from, ' saying ', msg);
+  });
 
-    socket.on('private message', function (from, msg) {
-          console.log('I received a private message by ', from, ' saying ', msg);
-          socket.emit('user disconnected');
-        });
-
-    socket.on('disconnect', function () {
-          io.emit('user disconnected');
-        });
+  socket.on('disconnect', function () {
+    console.log("user disconnect");
+  });
 });
