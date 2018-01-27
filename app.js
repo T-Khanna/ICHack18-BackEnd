c2d47f0db7133d31b8b6a303cafb922b718226ca
Ioannis Gabrielides <ioannisgabrielides@gmail.com>
2018-01-27 17:22:51 +0000

    Bind to port given in environment

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 
-var io = require('socket.io')(8080);
+var port = process.env.port || 8080;
+var io = require('socket.io')(port);
 
 var fs = require('fs');
 
 io.on('connection', function (socket) {
     io.emit('this', { will: 'be received by everyone'});
 
+    console.log("connected");
+
     socket.on('private message', function (from, msg) {
           console.log('I received a private message by ', from, ' saying ', msg);
+          socket.emit('user disconnected');
         });
 
     socket.on('disconnect', function () {
